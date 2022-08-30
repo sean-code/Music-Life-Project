cf5cfc27c75caf011e872c17b353f43d6237fb9e
sean-code <john.nganga@student.moringaschool.com>
2022-08-31 02:19:14 +0300

    Polish on the typo errors

--- src/index.js
+++ src/index.js
@@ -25,17 +25,17 @@ async function searchSongs(term) {
         .join("")}
     </ul>
    `;
-
+// Create Navigating Buttons For the Previous and Next Search Results
     if (data.prev || data.next) {
       forMore.innerHTML = `
         ${
           data.prev
-            ? `<button class="btn" onclick="getMoreSongs('${data.prev}')">Prev</button>`
+            ? `<button class="btn" onclick="getMoreSongs('${data.prev}')">PREVIOUS</button>`
             : ""
         }
         ${
           data.next
-            ? `<button class="btn" onclick="getMoreSongs('${data.next}')">Next</button>`
+            ? `<button class="btn" onclick="getMoreSongs('${data.next}')">NEXT</button>`
             : ""
         }
       `;
@@ -47,22 +47,22 @@ async function searchSongs(term) {
 
   // Fetching our songs from this server for more options of
   async function findMoreSongs(url) {
-    const response = await fetch(`https://cors-anywhere.herokuapp.com/${url}`);
+    const response = await fetch(`https://echo.hoppscotch.io/${url}`);
     const data = await response.json();
 
     showData(data);
   }
 
   async function getLyrics(artist, songTitle) {
-    const response = await fetch(`${apiURL}/v1/${artist}/${songTitle}`);
+    const response = await fetch(`${baseURL}/v1/${artist}/${songTitle}`);
     const data = await response.json();
 
     if (data.error) {
-      result.innerHTML = data.error;
+      forResult.innerHTML = data.error;
     } else {
       const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, "<br>");
 
-      result.innerHTML = `
+      forResult.innerHTML = `
       <h2><b>${artist}</b> - ${songTitle}</h2>
       <span>${lyrics}</span>
   `;
@@ -89,4 +89,16 @@ forForm.addEventListener('submit', (event) => {
 
 });
 
-// Adding the 
+// Adding the click event listener for getting the lyrics
+
+forResult.addEventListener("click", (event) => {
+  const clickedResult = event.target;
+
+  if (clickedResult.tagName === "BUTTON") {
+    const artist = clickedResult.getAttribute("data-artist");
+    const songTitle = clickedResult.getAttribute("data-songtitle");
+
+    getLyrics(artist, songTitle);
+  }
+});
+
