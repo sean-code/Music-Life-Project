244db4cfd52777b12291cf2095a07a31f7cb724c
sean-code <john.nganga@student.moringaschool.com>
2022-08-31 00:29:50 +0300

    add event listener to stop[ reloading of the page when the submit button is clicked

--- src/index.js
+++ src/index.js
@@ -2,3 +2,10 @@ const forForm = document.querySelector("#form");
 const forSearch = document.querySelector("#search");
 const forResult = document.querySelector("#result");
 const forMore = document.querySelector("#more");
+
+
+// Prevent Default Activity of The Form
+
+forForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+})
