0188f0046145819bbdeca3235995326b93720482
sean-code <john.nganga@student.moringaschool.com>
2022-08-31 00:12:38 +0300

    Declare variables using the DOM

---
+++ src/index.js
@@ -0,0 +1,4 @@
+const forForm = document.querySelector("#form");
+const forSearch = document.querySelector("#search");
+const forResult = document.querySelector("#result");
+const forMore = document.querySelector("#more");
