d9d4f0815dcd731bf158a4e9caa5b0591aab3afe
sean-code <john.nganga@student.moringaschool.com>
2022-08-31 00:32:20 +0300

    add the asynchronizing function for searching songs

--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ const forSearch = document.querySelector("#search");
 const forResult = document.querySelector("#result");
 const forMore = document.querySelector("#more");
 
+const baseURL = "https://api.lyrics.ovh";
+
+async function searchSongs(term) {
+    const response = await fetch(`${baseURL}/suggest/${term}`);
+    const data = await response.json();
+  
+    displayData(data);
+  }
+  
+
+  
 
 // Prevent Default Activity of The Form
 
