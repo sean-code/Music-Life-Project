75a5d32fbbbc29fe7a097ed4def8ef8debcdc0be
sean-code <john.nganga@student.moringaschool.com>
2022-09-01 12:21:42 +0300

    Use map method to display the fetched reviews from local server

--- src/comment.js
+++ src/comment.js
@@ -8,9 +8,20 @@ function getReviews(id){
     .then((data) =>{
         // console.log(data.reviews);
         reviews.innerHTML = data.reviews
+        .map(
+            (reviews) => `<li> 
+            ${reviews}
+            </li>`
+        )
+        .join("");
     })
 }
+// Add Event Listener that enables the loading of the webpage first before execution
+document.addEventListener('DOMContentLoaded', () =>{
+    getReviews(1);
+    getReviews(2);
 
+})
 
 // Adding Reviews
 // const inputName = document.getElementById('input-name');
